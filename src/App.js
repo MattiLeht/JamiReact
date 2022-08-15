6ed2a31aa6d3830543b9b74d2015f8048cf65f3c
Kajautus <jere.timonen@gmail.com>
2022-08-15 22:28:33 +0300

    Editd app.js
    app.js got styling, not explained yet, just in notes.txt
    To be continued.
    5:58

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
 function App() {
   return (
-    <div >
+    <div className='max-w-[728px] mx-auto text-center'>
+    // checkout notes.txt for explanations
+    <section className='flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative'>
+    </section>
 
     </div>
   );
