4949765c918e388f69b10f49e88ea53ec0d092ec
Joonas <109896862+Joonsar@users.noreply.github.com>
2022-08-18 12:32:00 +0300

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import SignIn from './SignIn'
+import LogOut from './LogOut'
+import {auth} from '../firebase'
+import {useAuthState} from 'react-firebase-hooks/auth'
+import tausta from './headerBG.jpg'
+
+
+
+const style = {
+    nav: `bg-white-800 h-13 flex justify-between items-center p-4`,
+    heading: `text-white text-xl`
+}
+
+const Navbar = () => {
+    const [user] = useAuthState(auth)
+    console.log(user)
+  return (
+    <div className={style.nav}>
+      <h1 className={style.heading}><img src={tausta}></img></h1>
+      {user ? <LogOut /> : <SignIn />}
+
+    </div>
+  );
+};
+
+export default Navbar;
