1068650bdb03ba0fa51f87795536e7b4754715b7
Joonas <109896862+Joonsar@users.noreply.github.com>
2022-08-18 18:09:53 +0300

    Käyttämättömät importit ym putsattu

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import {auth} from '../firebase'
 import buttonout from './buttonout2.jpg'
-import send from './buttonsend.jpg'
 
 const style = {
     button: `px-0 py-200 w-[37%] h-[37%]`,
-    
 }
 
 const LogOut = () => {
@@ -14,7 +12,6 @@ const signOut = () => {
 }
 
   return (
-    
     <button onClick={() => auth.signOut()} className={style.button}><img src={buttonout}></img>
     </button>
   )
